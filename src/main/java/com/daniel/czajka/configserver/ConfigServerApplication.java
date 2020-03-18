faf22d0f07343b8e77fb2e31fce834c1aa3278b9
Daniel Czajka <daniel.czajka@gmail.com>
2020-03-18 21:23:45 +0100

    working on config server

--- src/main/java/com/daniel/czajka/configserver/ConfigServerApplication.java
+++ src/main/java/com/daniel/czajka/configserver/ConfigServerApplication.java
@@ -2,9 +2,22 @@ package com.daniel.czajka.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
